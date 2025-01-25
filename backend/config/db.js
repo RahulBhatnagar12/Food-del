c282007a52425e302bb95d183a95c3b7100e73d4
Rahul Bhatnagar <rahulbhatnagar364@gmail.com>
2025-01-25 20:09:40 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-<<<<<<< HEAD
+
     await mongoose.connect('mongodb+srv://Food-del:Fooddel123@cluster0.z3f2v.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-=======
-    await mongoose.connect('{ Add your mongodb URI Here }/food-del').then(()=>console.log("DB Connected"));
->>>>>>> 1e11d4fccf5a48e58c5b8a513154a809c6af39f2
+
+
    
 }
 
